c3389f6d3b57558d1d454abb45d303614cf64302
jonibek99 <jonibekannaev5@gmail.com>
2025-12-24 16:44:07 +0500

    solo

--- solo/vizulation.py
+++ solo/vizulation.py
@@ -1,43 +1,70 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-python_group='7'
-# number='0'
-when='daily'
-daily=f"python_{python_group}_daily"
-weekly=f"python_{python_group}_weekly"
-monthly=f"python_{python_group}_monthly"
-total_ll=f'python_{python_group}_total_all'
-
-df=pd.read_csv(f'data/results/{monthly}.csv')
-
-fig, ax = plt.subplots(2,2,figsize=(20, 8))
-fig.suptitle('Python_Codewars_results')
-plt.xticks(rotation=90)
-sns.lineplot(data=df,ax=ax[0,0],x=df.username,y=df.total_completed,color='red').grid(which='both')
-
-plt.ylabel('katalar_soni')
-plt.xlabel('foydananuvchilar')
-sns.barplot(data=df,ax=ax[0,1],x=df.username,y=df.total_completed,color='black').grid(which='both')
-sns.histplot(data=df,ax=ax[1,0],x=df.username,y=df.total_completed,color='green').grid(which='both')
-sns.boxplot(data=df,ax=ax[1,1],x=df.username,y=df.total_completed,color='blue').grid(which='both')
-plt.ylabel('katalar_soni')
-plt.xlabel('foydananuvchilar')
-for a in ax.flat:
-    a.tick_params(axis='x', rotation=90)
+import os
+daily='daily'
+weekly='weekly'
+monthly='monthly'
+total_all='total_all'
+python_group = '8'
 
 
-for i in python_group:
-    if i in python_group:
-        plt.savefig(f'pictures/codewars_results/python_{python_group}.svg')
+when = daily # Bu yerni o'zgartiring: 'daily', 'weekly', yoki 'monthly'
+
+# when qiymatiga qarab fayl nomini aniqlash
+if when == 'daily':
+    file_name = f"python_{python_group}_daily"
+elif when == 'weekly':
+    file_name = f"python_{python_group}_weekly"
+elif when == 'monthly':
+    file_name = f"python_{python_group}_monthly"
+else:
+    file_name = f"python_{python_group}_daily"  # default
+
+df = pd.read_csv(f'data/results/{file_name}.csv')
+
+# Papkalarni yaratish
+#os.makedirs('pictures/codewars_results', exist_ok=True)
+os.makedirs(f'when/{when}', exist_ok=True)  # when papkasiga qarab alohida papka
+
+fig, ax = plt.subplots(2, 2, figsize=(40, 15))
+fig.suptitle(f'Python Codewars Results - {when.capitalize()}')
+
+# 1. Line plot
+sns.lineplot(data=df, ax=ax[0, 0], x='username', y='total_completed', color='red')
+ax[0, 0].grid(which='both')
+ax[0, 0].set_ylabel('katalar_soni')
+ax[0, 0].set_xlabel('foydananuvchilar')
+ax[0, 0].tick_params(axis='x', rotation=90)
+
+# 2. Bar plot
+sns.barplot(data=df, ax=ax[0, 1], x='username', y='total_completed', color='black')
+ax[0, 1].grid(which='both')
+ax[0, 1].set_ylabel('katalar_soni')
+ax[0, 1].set_xlabel('foydananuvchilar')
+ax[0, 1].tick_params(axis='x', rotation=90)
+
+# 3. Histplot
+sns.histplot(data=df['total_completed'], ax=ax[1, 0], color='green', kde=True)
+ax[1, 0].grid(which='both')
+ax[1, 0].set_ylabel('soni')
+ax[1, 0].set_xlabel('katalar_soni')
+ax[1, 0].tick_params(axis='x', rotation=90)
+
+# 4. Boxplot
+sns.boxplot(data=df, ax=ax[1, 1], x='username', y='total_completed', color='blue')
+ax[1, 1].grid(which='both')
+ax[1, 1].set_ylabel('katalar_soni')
+ax[1, 1].set_xlabel('foydananuvchilar')
+ax[1, 1].tick_params(axis='x', rotation=90)
+
+# Ikkala joyga saqlash
+#plt.savefig(f'pictures/codewars_results/python_{python_group}_{when}.svg', bbox_inches='tight')
+plt.savefig(f'when/{when}/python_{python_group}.svg', bbox_inches='tight')
 
 plt.tight_layout()
 plt.show()
 
-
-
-
-
-
-
+print(f"Grafiklar saqlandi:")
+#print(f"1. pictures/codewars_results/python_{python_group}_{when}.svg")
+print(f"2. when/{when}/python_{python_group}.svg")
